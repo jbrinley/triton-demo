386aeed29072f66842eaa7a86c5a70c4a7fc4e24
Jonathan Brinley <jbrinley@flightless.us>
2015-07-27 10:30:26 -0400

    add stop script

---
+++ dev/docker/stop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$SCRIPTDIR"
+
+$(cat ~/.sdc/docker/moderntribe/env.sh)
+
+# use a local docker-compose to ensure version 1.2
+./docker-compose --project-name=wordpress --file=production.yml stop
